6cc9dee935ca3646809ea8aef8fc09f63cd7b33c
SeldomseenSchweig <jamesschweig@gmail.com>
2024-02-16 12:11:07 -0800

    adding routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/dogs", (req, res) => {
+  res.send("<h1>HELLO</h1>");
+});
+
 app.listen(3001, () => {
   console.log("running on port 3001");
 });
