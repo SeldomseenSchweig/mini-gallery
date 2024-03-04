0914503ff5ef6313f22f024b64b0d8a6110ef632
SeldomseenSchweig <jamesschweig@gmail.com>
2024-03-04 11:47:40 -0800

    made backend user creation

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,8 +2,21 @@ const express = require("express");
 const db = require("../db");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get("/", async (req, res, next) => {
   const results = await db.query(`SELECT * FROM users`);
   return res.json(results.rows);
 });
 module.exports = router;
+
+router.post("/", async (req, res, next) => {
+  try {
+    let { username, email, password } = req.body;
+    const result = await db.query(
+      "INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING  *",
+      [username, email, password]
+    );
+    return res.status(201).json(result.rows[0]);
+  } catch (error) {
+    return next(error);
+  }
+});
