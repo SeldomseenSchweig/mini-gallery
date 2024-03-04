31e58f09e63efc44753aaf5f310cccc0a60c131b
SeldomseenSchweig <jamesschweig@gmail.com>
2024-03-04 11:59:33 -0800

    added password hashing

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,3 +1,4 @@
+const { hash, compare } = require("bcrypt");
 const express = require("express");
 const db = require("../db");
 const router = express.Router();
@@ -11,9 +12,10 @@ module.exports = router;
 router.post("/", async (req, res, next) => {
   try {
     let { username, email, password } = req.body;
+    let hashedPassword = await hash(password, 12);
     const result = await db.query(
       "INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING  *",
-      [username, email, password]
+      [username, email, hashedPassword]
     );
     return res.status(201).json(result.rows[0]);
   } catch (error) {
